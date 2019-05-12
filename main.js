53f60578a52faab3a39207e74afa1b749f072fc4
shirleywbi <swwbi@msn.com>
2019-05-11 17:23:57 -0700

    basic setup for about/main/main.js/style.css

---
+++ main.js
@@ -0,0 +1,15 @@
+function clearContent() {
+  
+}
+
+function clearAll() {
+  // TODO
+}
+
+function addToTable() {
+  let item = document.getElementById('item').value; // debug this
+  let node = document.createElement('li');
+  let textnode = document.createTextNode(item);
+  node.appendChild(textnode);
+  document.getElementById('table').appendChild(node);
+}
