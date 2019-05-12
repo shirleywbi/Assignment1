cceafb6fd27e61a30442d69783f5ec190ba9f13e
shirleywbi <swwbi@msn.com>
2019-05-12 14:38:55 -0700

    add name functionality

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
- let msgs = '[{"text":"We\'re no strangers to love. '
+ let msgs = '[{"name": "Rick", "text":"We\'re no strangers to love. '
 + 'You know the rules and so do I. '
 + 'A full commitment\'s what I\'m thinking of. '
 + 'You wouldn\'t get this from any other guy."},'
-+ '{"text":"I just wanna tell you how I\'m feeling... '
++ '{"name": "Richard", "text":"I just wanna tell you how I\'m feeling... '
 + 'Gotta make you understand..."},'
-+ '{"text":"Never gonna give you up. '
++ '{"name": "Paul", "text":"Never gonna give you up. '
 + 'Never gonna let you down. '
 + 'Never gonna run around and desert you. '
 + 'Never gonna make you cry. '
 + 'Never gonna say goodbye."},'
-+ '{"text":"Never gonna tell a lie and hurt you."}]';
++ '{"name": "Rick Astley", "text":"Never gonna tell a lie and hurt you."}]';
 
 function loadMessages() {
   msgs = JSON.parse(msgs);
   console.log(msgs); 
   for (let i in msgs) {
     console.log(msgs[i].text);
-    addMessage(msgs[i].text);
+    addMessage(msgs[i].name, msgs[i].text);
   }
 };
 
-function clearTextArea() {
+function clearForm() {
+  document.getElementById('name').value = "";
   document.getElementById('new-msg').value = "";
 }
 
@@ -32,14 +33,16 @@ function addNewMessage() {
   try {
     let text = document.getElementById('new-msg').value;
     if (text === "") return;
-    addMessage(text);
-    clearTextArea();
+    let name = getName();
+    addMessage(name, text);
+    clearForm();
   } catch (err) {
     console.log(err);
   }
 }
 
-function addMessage(msg) {
+function addMessage(name, msg) {
+  msg = name + ' says: "' + msg + '"';
   let node = document.createElement('li');
   node.className = "msg";
   let textnode = document.createTextNode(msg);
@@ -48,10 +51,15 @@ function addMessage(msg) {
   addDeleteButton(node);
 }
 
+function getName() {
+  let name = document.getElementById('name').value;
+  return name !== "" ? name : "Anonymous";
+}
+
 function addDeleteButton(node) {
   let button = document.createElement('button');
   button.textContent = "x";
-  button.className = "msg-delete-btn button";
+  button.className = "small-button button";
   button.addEventListener('click', deleteMessage);
   node.appendChild(button);
 }
