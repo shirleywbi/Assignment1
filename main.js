1cab3bb63c18a4c12295ef92d8b88093df9f705d
shirleywbi <swwbi@msn.com>
2019-05-12 01:51:14 -0700

    moved initial list to JSON in JS file

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let msgs = '[{"text":"Never gonna give you up"},'
+            + '{"text":"Never gonna let you down"},'
+            + '{"text":"Never gonna run around and desert you"},'
+            + '{"text":"Never gonna make you cry"}]';
+
+function loadMessages() {
+  msgs = JSON.parse(msgs);
+  console.log(msgs); 
+  for (let i in msgs) {
+    console.log(msgs[i].text);
+    addMessage(msgs[i].text);
+  }
+};
+
+// window.onload = loadMessages;
+
 function clearTextArea() {
   document.getElementById('new-msg').value = "";
   // TODO: Debug - Clear All > Clear adds back message board 
@@ -8,17 +24,21 @@ function clearAll() {
   document.getElementById('msg-list').innerHTML = "";
 }
 
-function addToTable() {
+function addNewMessage() {
   try {
     let text = document.getElementById('new-msg').value;
     if (text === "") return;
-    let node = document.createElement('li');
-    node.className = "msg";
-    let textnode = document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById('msg-list').appendChild(node);
+    addMessage(text);
     clearTextArea();
   } catch (err) {
     console.log(err);
   }
+}
+
+function addMessage(msg) {
+  let node = document.createElement('li');
+  node.className = "msg";
+  let textnode = document.createTextNode(msg);
+  node.appendChild(textnode);
+  document.getElementById('msg-list').appendChild(node);
 }
