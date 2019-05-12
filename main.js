4e1fd140684b399013d4bf87910ca5aa13f61c66
shirleywbi <swwbi@msn.com>
2019-05-12 00:31:49 -0700

    debug addToTable() - form was refreshing

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-function clearContent() {
-  
+function clearTextArea() {
+  document.getElementById('new-msg').innerText = "";
+  // TODO: Debug - Clear All > Clear adds back message board 
+  // -- might be b/c it is in form? refreshes everything?
 }
 
 function clearAll() {
-  // TODO
+  document.getElementById('msg-list').innerHTML = "";
 }
 
 function addToTable() {
-  let item = document.getElementById('new-item').value; // debug this
-  let node = document.createElement('li');
-  let textnode = document.createTextNode(item);
-  node.appendChild(textnode);
-  document.getElementById('table').appendChild(node);
+  try {
+    let text = document.getElementById('new-msg').value;
+    if (text === "") return;
+    let node = document.createElement('li');
+    node.className = "msg";
+    let textnode = document.createTextNode(text);
+    node.appendChild(textnode);
+    document.getElementById('msg-list').appendChild(node);
+    console.log(document.getElementById('msg-list'));
+    console.log(text);
+  } catch (err) {
+    console.log(err);
+  }
 }
